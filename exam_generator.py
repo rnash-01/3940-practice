c6912627cd5cd8715c5865d6278d2c37b1d308ff
Raphael Nash <sc20rn@leeds.ac.uk>
2023-01-22 22:40:38 +0000

    make interface bit cleaner

--- exam_generator.py
+++ exam_generator.py
@@ -1,18 +1,9 @@
 import random
-# Helper functions
+from maxflow import *
+from matchings import *
+from complexity import *
 
-def compute_capacities(G):
-    (V,E) = G
-
-    # First, calculate the general flow
-
-    capacities = {}
-    for e in E:
-        capacities[e] = random.randint(1, 10)
-
-    return capacities
-
-# Question generation
+# Tex file
 def init_tex_file():
     f = open("example_test.tex", "w")
     f.write(
@@ -41,59 +32,48 @@ def end_tex_file(f):
     f.write("\\end{document}")
     f.close()
 
-
-def max_flow_question(f):
-
-    # Get graph
-    V = [1,2,3,4,5,6,7,8]
-    E = {(1, 2), (1,4), (1,6), (2,3), (4,5), (6,7), (4,2), (4,6), (3,5), (7,5), (3,8), (5,8), (7,8)}
-    G = (V, E)
-    capacities = compute_capacities(G)
-
-    # Sort out latex
-    f.write("Find the maximum flow in the graph below\n")
-    f.write(
-"""\\begin{center}
-  \\begin{tikzpicture}[scale=0.8, >=stealth]
-    \\node[v, label=left :s] (s) at (0,2) {}; 
-    \\foreach \\n/\\y in {a/0, b/2, c/4} \\node[v] (\\n) at (2,\\y) {};
-    \\foreach \\n/\\y in {d/0, e/2, f/4} \\node[v] (\\n) at (4,\\y) {};
-    \\node[v, label=right:t] (t) at (6,2) {};
-""")
-    f.write(f"""
-    \\draw[->] (s) to[edge label'={capacities[(1,2)]}] (a);
-    \\draw[->] (s) to[edge label ={capacities[(1,4)]}] (b);
-    \\draw[->] (s) to[edge label ={capacities[(1,6)]}] (c);
-    \\draw[->] (b) to[edge label'={capacities[(4,2)]}] (a);
-    \\draw[->] (b) to[edge label ={capacities[(4,6)]}] (c);
-    \\draw[->] (a) to[edge label ={capacities[(2,3)]}] (d);
-    \\draw[->] (b) to[edge label ={capacities[(4,5)]}] (e);
-    \\draw[->] (c) to[edge label ={capacities[(6,7)]}] (f);
-    \\draw[->] (d) to[edge label ={capacities[(3,5)]}] (e);
-    \\draw[->] (f) to[edge label'={capacities[(7,5)]}] (e);
-    \\draw[->] (d) to[edge label'={capacities[(3,8)]}] (t);
-    \\draw[->] (e) to[edge label ={capacities[(5,8)]}] (t);
-    \\draw[->] (f) to[edge label ={capacities[(7,8)]}] (t);"""
-    )
-
-    f.write(
-"""\\end{tikzpicture}
-\\end{center}\n"""
-    )
-
+# Question generation
 
 def network_flow(f):
     f.write("\\section{Network Flow}\n")
-    questions = {
-        "Max Flow": max_flow_question
-    }
+
+    # ~~~ PRACTICE QUESTIONS FOR NETWORK FLOW GO HERE ~~~
+    questions = [max_flow_question]     # append your function names from maxflow.py here
     
-    for title in questions:
-        questions[title](f)
+    for question in questions:
+        questions(f)
+
+def matchings(f):
+    f.write("\\section{Matchings}\n")
+
+    # ~~~ PRACTICE QUESTIONS FOR MATCHINGS GO HERE ~~~
+    questions = [max_flow_question]     # append your function names from matchings.py here
     
+    for question in questions:
+        questions(f)
+    
+    f.write("\n")
+    
+def complexity(f):
+    f.write("\\section{Complexity Theory}\n")
 
-
-
+    # ~~~ PRACTICE QUESTIONS FOR COMPLEXITY THEORY GO HERE ~~~
+    questions = [max_flow_question]     # append your function names from complexity.py here
+    
+    for question in questions:
+        questions(f)
+    
+# MAIN SCRIPT
 f = init_tex_file()
+
+# SECTION 1 - NETWORK FLOW
 network_flow(f)
+
+# SECTION 2 - MATCHINGS
+matchings(f)
+
+# SECTION 3 - COMPLEXITY THEORY
+complexity(f)
+
+# End tex file
 end_tex_file(f)
